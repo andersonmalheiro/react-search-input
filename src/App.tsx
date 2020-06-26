f39237ad1d1c1a5f83f08446bcf019d0a563e68c
Anderson <anderson.malheiro@aluno.fapce.edu.br>
2020-06-26 16:50:17 -0300

    [master] - Selecting an option

--- src/App.tsx
+++ src/App.tsx
@@ -13,18 +13,24 @@ function App() {
 
   const [searching, setSearching] = React.useState<boolean>(false);
 
+  const [showOptions, setShowOptions] = React.useState<boolean>(false);
+
   const doSearch = (value: string) => {
+    console.log('doSearchValue ->', value);
     const tmp = fruits.filter((f) => f.toLowerCase().includes(value));
+    console.log('results ->', tmp);
     setResults(tmp);
     setSearching(false);
   };
 
   const handleSearch = (event: ChangeEvent) => {
     event.preventDefault();
+    setShowOptions(true);
     const input = event.target as HTMLInputElement;
 
     if (input) {
       const value = input.value;
+      console.log(value);
       setSearch(value);
       setSearching(true);
       const debounced = debounce(doSearch, 1000);
@@ -32,22 +38,41 @@ function App() {
     }
   };
 
+  const handleSelect = (result: any) => {
+    setSearch(result);
+    setShowOptions(false);
+    setResults([]);
+  };
+
+  const handleFocus = (event: any) => {
+    if (search.length === 0) {
+      setResults(fruits);
+      setShowOptions(true);
+    }
+  }
+
   return (
     <div className="container">
       <input
         ref={inputRef}
         type="text"
+        value={search}
         placeholder="Search..."
         onChange={(event) => handleSearch(event)}
+        onFocus={(event) => handleFocus(event)}
       />
 
-      {search && (
+      {showOptions && (
         <div className="results">
           {searching && <span>Loading...</span>}
           {!searching && (
             <ul>
               {results &&
-                results.map((result, index) => <li key={index}>{result}</li>)}
+                results.map((result, index) => (
+                  <li onClick={() => handleSelect(result)} key={index}>
+                    {result}
+                  </li>
+                ))}
             </ul>
           )}
         </div>
