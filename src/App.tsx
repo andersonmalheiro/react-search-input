52559838d3c4dca665bb7f55f5fc8e09849a37fc
Anderson <anderson.malheiro@aluno.fapce.edu.br>
2020-06-26 16:12:19 -0300

    [master] - Initial commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,57 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { ChangeEvent } from "react";
+import "./App.scss";
+import { debounce } from "./utils";
 
 function App() {
+  const inputRef = React.useRef<HTMLInputElement>(null);
+
+  const fruits = ["Banana", "Maçã", "Mamão", "Laranja", "Abacate"];
+
+  const [search, setSearch] = React.useState<string>("");
+
+  const [results, setResults] = React.useState<string[]>([]);
+
+  const [searching, setSearching] = React.useState<boolean>(false);
+
+  const doSearch = (value: string) => {
+    const tmp = fruits.filter((f) => f.toLowerCase().includes(value));
+    setResults(tmp);
+    setSearching(false);
+  };
+
+  const handleSearch = (event: ChangeEvent) => {
+    event.preventDefault();
+    const input = event.target as HTMLInputElement;
+
+    if (input) {
+      const value = input.value;
+      setSearch(value);
+      setSearching(true);
+      const debounced = debounce(doSearch, 1000);
+      debounced(value);
+    }
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <input
+        ref={inputRef}
+        type="text"
+        placeholder="Search..."
+        onChange={(event) => handleSearch(event)}
+      />
+
+      {search && (
+        <div className="results">
+          {searching && <span>Loading...</span>}
+          {!searching && (
+            <ul>
+              {results &&
+                results.map((result, index) => <li key={index}>{result}</li>)}
+            </ul>
+          )}
+        </div>
+      )}
     </div>
   );
 }
